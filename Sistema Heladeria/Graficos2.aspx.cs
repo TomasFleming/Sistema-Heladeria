be0ad8d36664c58ea5d128b0675f17d5c7e8ca3c
Tomas Fleming <TomasFleming43@outlook.es>
2023-11-01 17:03:14 -0300

    Mejoras visuales

--- Sistema Heladeria/Graficos2.aspx.cs
+++ Sistema Heladeria/Graficos2.aspx.cs
@@ -8,6 +8,8 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Net;
 using System.Web.UI.DataVisualization.Charting;
+using System.Drawing;
+
 namespace Sistema_Heladeria
 {
     public partial class Graficos2 : System.Web.UI.Page
@@ -82,101 +84,82 @@ namespace Sistema_Heladeria
 
             con.Close();
 
-            //Ahora para Ingresos
-
             con.Open();
-            String rangoE = @"WITH Meses AS (
-                    SELECT 1 AS MesNumero, 'Enero' AS NombreMes
-                    UNION SELECT 2, 'Febrero'
-                    UNION SELECT 3, 'Marzo'
-                    UNION SELECT 4, 'Abril'
-                    UNION SELECT 5, 'Mayo'
-                    UNION SELECT 6, 'Junio'
-                    UNION SELECT 7, 'Julio'
-                    UNION SELECT 8, 'Agosto'
-                    UNION SELECT 9, 'Septiembre'
-                    UNION SELECT 10, 'Octubre'
-                    UNION SELECT 11, 'Noviembre'
-                    UNION SELECT 12, 'Diciembre'
-                )
-                , VentasGanancias AS (
-                    SELECT
-                        DATEPART(MONTH, Fecha) AS MesNumero,
-                        SUM(Total) AS Ingresos
-                    FROM Ventas
-                    WHERE YEAR(Fecha) = 2023
-                    GROUP BY DATEPART(MONTH, Fecha)
-                )
-                SELECT
-                    Meses.NombreMes,
-                    ISNULL(Ingresos, 0) AS Ganancias
-                FROM Meses
-                LEFT JOIN VentasGanancias ON Meses.MesNumero = VentasGanancias.MesNumero
-                WHERE Meses.MesNumero BETWEEN @MesInicio AND @MesFin
-                ORDER BY Meses.MesNumero";
 
-            SqlCommand sqlE = new SqlCommand(rangoE, con.GetConnection());
-            sqlE.Parameters.Add(new SqlParameter("@MesInicio", mesInicio));
-            sqlE.Parameters.Add(new SqlParameter("@MesFin", mesFin));
+            // Consulta para Ingresos
+            string consultaIngresos = @"WITH Meses AS (
+    SELECT 1 AS MesNumero, 'Enero' AS NombreMes
+    UNION SELECT 2, 'Febrero'
+    UNION SELECT 3, 'Marzo'
+    UNION SELECT 4, 'Abril'
+    UNION SELECT 5, 'Mayo'
+    UNION SELECT 6, 'Junio'
+    UNION SELECT 7, 'Julio'
+    UNION SELECT 8, 'Agosto'
+    UNION SELECT 9, 'Septiembre'
+    UNION SELECT 10, 'Octubre'
+    UNION SELECT 11, 'Noviembre'
+    UNION SELECT 12, 'Diciembre'
+), VentasGanancias AS (
+    SELECT
+        DATEPART(MONTH, Fecha) AS MesNumero,
+        SUM(Total) AS Ingresos
+    FROM Ventas
+    WHERE YEAR(Fecha) = 2023
+    GROUP BY DATEPART(MONTH, Fecha)
+), PagosGanancias AS (
+    SELECT
+        DATEPART(MONTH, Fecha_Pago) AS MesNumero,
+        SUM(Total) AS Salidas
+    FROM Registro_Pagos
+    WHERE YEAR(Fecha_Pago) = 2023
+    GROUP BY DATEPART(MONTH, Fecha_Pago)
+)
 
-            using (SqlDataReader Ingresos = sqlE.ExecuteReader())
+SELECT
+    Meses.NombreMes,
+    ISNULL(Ingresos, 0) AS Ingresos,
+    ISNULL(Salidas, 0) AS Salidas
+FROM Meses
+LEFT JOIN VentasGanancias ON Meses.MesNumero = VentasGanancias.MesNumero
+LEFT JOIN PagosGanancias ON Meses.MesNumero = PagosGanancias.MesNumero
+WHERE Meses.MesNumero BETWEEN @MesInicio AND @MesFin
+ORDER BY Meses.MesNumero;";
+
+            SqlCommand cmd = new SqlCommand(consultaIngresos, con.GetConnection());
+            cmd.Parameters.Add(new SqlParameter("@MesInicio", mesInicio));
+            cmd.Parameters.Add(new SqlParameter("@MesFin", mesFin));
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                while (Ingresos.Read())
+                Series Ingresos = GraficoComparativo.Series["Series1"];
+                Ingresos.ChartType = SeriesChartType.Column;
+
+                Series Salidas = GraficoComparativo.Series["Series2"];
+                Salidas.ChartType = SeriesChartType.Column;
+
+                while (reader.Read())
                 {
-                    Series Datos = GraficoIngresos.Series["Series1"];
-                    Datos.Points.AddXY(Ingresos["NombreMes"].ToString(), Ingresos["Ganancias"]);
+                    string nombreMes = reader["NombreMes"].ToString();
+                    float ingresos = Convert.ToInt32(reader["Ingresos"]);
+                    float salidas = Convert.ToInt32(reader["Salidas"]);
+
+                    Ingresos.Points.AddXY(nombreMes, ingresos);
+                    Salidas.Points.AddXY(nombreMes, salidas);
                 }
+                Color colorIngresos = Color.Green; // Puedes cambiar esto al color que desees.
+                Color colorSalidas = Color.Red;    // Puedes cambiar esto al color que desees.
+
+                // Asigna los colores a las series.
+                Ingresos.Color = colorIngresos;
+                Salidas.Color = colorSalidas;
+
+                GraficoComparativo.ChartAreas[0].AxisY.LabelStyle.Format = "C2";
+
             }
 
             con.Close();
 
-            //ahora para perdidas
-            con.Open();
-            String rangoP = @"WITH Meses AS (
-                    SELECT 1 AS MesNumero, 'Enero' AS NombreMes
-                    UNION SELECT 2, 'Febrero'
-                    UNION SELECT 3, 'Marzo'
-                    UNION SELECT 4, 'Abril'
-                    UNION SELECT 5, 'Mayo'
-                    UNION SELECT 6, 'Junio'
-                    UNION SELECT 7, 'Julio'
-                    UNION SELECT 8, 'Agosto'
-                    UNION SELECT 9, 'Septiembre'
-                    UNION SELECT 10, 'Octubre'
-                    UNION SELECT 11, 'Noviembre'
-                    UNION SELECT 12, 'Diciembre'
-                )
-                ,PagosGanancias AS (
-                    SELECT
-                        DATEPART(MONTH, Fecha_Pago) AS MesNumero,
-                        SUM(Total) AS Salidas
-                    FROM Registro_Pagos
-                    WHERE YEAR(Fecha_Pago) = 2023
-                    GROUP BY DATEPART(MONTH, Fecha_Pago)
-                )
-
-                SELECT
-                    Meses.NombreMes,
-                     ISNULL(Salidas, 0) AS Ganancias
-                FROM Meses
-                LEFT JOIN PagosGanancias ON Meses.MesNumero = PagosGanancias.MesNumero
-                WHERE Meses.MesNumero BETWEEN @MesInicio AND @MesFin
-                ORDER BY Meses.MesNumero";
-
-            SqlCommand sql2 = new SqlCommand(rangoP, con.GetConnection());
-            sql2.Parameters.Add(new SqlParameter("@MesInicio", mesInicio));
-            sql2.Parameters.Add(new SqlParameter("@MesFin", mesFin));
-
-            using (SqlDataReader Salidas = sql2.ExecuteReader())
-            {
-                while (Salidas.Read())
-                {
-                    Series Datos = GraficoSalidas.Series["Series1"];
-                    Datos.Points.AddXY(Salidas["NombreMes"].ToString(), Salidas["Ganancias"]);
-                }
-            }
-
-            con.Close();
         }
 
         protected void Lipiar_Ganancias_btn_Click(object sender, EventArgs e)
